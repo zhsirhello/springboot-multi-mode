1513e2171ffe449a2577f7e1607f824eb3f8b514
zhsirhello <281797294@qq.com>
2020-03-25 13:33:16 +0800

    mybatis反向工程

---
+++ zjj-mybatis-generater/src/main/java/com/zjj/zjjmybatisgenerater/Generator.java
@@ -0,0 +1,34 @@
+package com.zjj.zjjmybatisgenerater;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+    public void generator() throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        /**指向逆向工程的配置文件*/
+        File configFile = new File("E:\\ideawork\\springboot-multi-model\\zjj-mybatis-generater\\src\\main\\resources\\generationConfig.xml");
+        //File configFile = new File()
+        ConfigurationParser parser = new ConfigurationParser(warnings);
+        Configuration config = parser.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+    public static void main(String[] args) throws Exception {
+        try {
+            Generator generatorSqlmap = new Generator();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
