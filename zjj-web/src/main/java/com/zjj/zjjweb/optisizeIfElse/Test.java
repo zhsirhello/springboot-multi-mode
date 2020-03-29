0dc91b097a00ca5aee1ca03630e5adafdb87fe2b
zhsirhello <281797294@qq.com>
2020-03-29 14:58:48 +0800

    使用枚举优化多重if，switch语句

--- zjj-web/src/main/java/com/zjj/zjjweb/optisizeIfElse/Test.java
+++ zjj-web/src/main/java/com/zjj/zjjweb/optisizeIfElse/Test.java
@@ -1,5 +1,10 @@
 package com.zjj.zjjweb.optisizeIfElse;
 
+/**
+ * 诸如if A
+ *     else if B
+ *     else if C
+ */
 public class Test {
     public static void main(String[] args){
         RoleEnum.valueOf("A").execute("gogogo");
