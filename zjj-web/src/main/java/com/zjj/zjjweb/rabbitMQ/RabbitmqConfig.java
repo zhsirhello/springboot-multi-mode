11d34073a8f138836d0468102fd7b487b31d4bb4
zhsirhello <281797294@qq.com>
2020-03-29 11:03:41 +0800

    springboot整合rabbitmq的一个示范

--- zjj-web/src/main/java/com/zjj/zjjweb/rabbitMQ/RabbitmqConfig.java
+++ zjj-web/src/main/java/com/zjj/zjjweb/rabbitMQ/RabbitmqConfig.java
@@ -2,6 +2,7 @@ package com.zjj.zjjweb.rabbitMQ;
 
 
 import com.google.common.collect.Maps;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
@@ -9,6 +10,8 @@ import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFacto
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
@@ -176,6 +179,41 @@ public class RabbitmqConfig {
     @Bean
     public Binding executeLimitBinding(){
         return BindingBuilder.bind(executeLimitQueue()).to(executeLimitExchange()).with(env.getProperty("mq.kill.item.execute.limit.queue.routing.key"));
+    }
+
+    /**
+     * 另外一种写法，在这个里面就处理消息了！！！！
+     * @return
+     */
+    @Bean
+    public SimpleMessageListenerContainer messageContainer() {
+        //加载处理消息A的队列
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory);
+        //设置接收多个队列里面的消息，这里设置接收队列A
+        //假如想一个消费者处理多个队列里面的信息可以如下设置：
+        //container.setQueues(queueA(),queueB(),queueC());
+        //container.setQueues(queueA());
+        container.setExposeListenerChannel(true);
+        //设置最大的并发的消费者数量
+        container.setMaxConcurrentConsumers(10);
+        //最小的并发消费者的数量
+        container.setConcurrentConsumers(1);
+        //设置确认模式手工确认
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        container.setMessageListener(new ChannelAwareMessageListener() {
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                /**通过basic.qos方法设置prefetch_count=1，这样RabbitMQ就会使得每个Consumer在同一个时间点最多处理一个Message，
+                 换句话说,在接收到该Consumer的ack前,它不会将新的Message分发给它 */
+                channel.basicQos(1);
+                byte[] body = message.getBody();
+                //logger.info("接收处理队列A当中的消息:" + new String(body));
+                /**为了保证永远不会丢失消息，RabbitMQ支持消息应答机制。
+                 当消费者接收到消息并完成任务后会往RabbitMQ服务器发送一条确认的命令，然后RabbitMQ才会将消息删除。*/
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            }
+        });
+        return container;
     }
 }
 
